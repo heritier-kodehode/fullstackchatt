5c5fd3f0c4b30c20d2eaa31e53ecd076524c3cb0
heritier-kodehode <heritier.kodehode@gmail.com>
2022-11-25 09:54:42 +0100

    added full crud functionality on the backend wiht mongodb

--- backendreactchatt/controllers/userController.js
+++ backendreactchatt/controllers/userController.js
@@ -13,29 +13,49 @@ const getUsers = asyncHandler(async (req, res) => {
 //@route SET /api/users
 //@access Private
 const setUsers = asyncHandler(async (req, res) => {
-  if (!req.body.text) {
+  if (!req.body.username) {
     res.status(400);
     throw new Error("Please add a text field");
   }
   const user = await User.create({
-    username: req.body.text,
+    username: req.body.username,
   });
 
-  res.status(200).json({ message: "Set USERS" });
+  res.status(200).json(user);
 });
 
 //@desc Update user
 //@route Update /api/users/:id
 //@access Private
 const updateUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update USER ${req.params.id}` });
+  const user = await User.findById(req.params.id);
+
+  if (!user) {
+    res.status(400);
+    throw new Error("User not found");
+  }
+
+  const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json(updatedUser);
 });
 
 //@desc Delete user
 //@route Detele /api/users/:id
 //@access Private
 const deleteUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Delete USER" + req.params.id });
+  const user = await User.findById(req.params.id);
+
+  if (!user) {
+    res.status(400);
+    throw new Error("User not found");
+  }
+
+  await user.remove();
+
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
